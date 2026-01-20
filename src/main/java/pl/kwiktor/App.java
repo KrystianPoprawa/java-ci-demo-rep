9a1958829a6dbdb0eb133fc857605ee8702a3eaa
Krystian Poprawa <39667626+KrystianPoprawa@users.noreply.github.com>
2026-01-20 20:28:30 +0100

    Update1 App.java

--- src/main/java/pl/kwiktor/App.java
+++ src/main/java/pl/kwiktor/App.java
@@ -3,6 +3,7 @@ package pl.kwiktor;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello CI!");
+        String password = "admin";
     }
 
     public int add(int a, int b) {
